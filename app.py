e08dac3163782c4ab2d9a7c077ee5c41acd69cda
Kanok KG <porupo706@gmail.com>
2026-09-08 22:00:01 +0700

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from PyPDF2 import PdfReader
 from langchain_core.documents import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
